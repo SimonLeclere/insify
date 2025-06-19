9f99ade88c23a8533136111f93da43edf2080ee2
Simon Leclere <simon.leclere@mantra.ms>
2025-06-19 13:55:07 +0200

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,6 @@ export default async function RootLayout({
           attribute="class"
           defaultTheme="system"
           enableSystem
-          disableTransitionOnChange
         >
           {children}
           <Toaster richColors/>          
--- components/ui/collapsible.tsx
+++ components/ui/collapsible.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { cn } from "@/lib/utils";
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
 
 function Collapsible({
@@ -19,15 +20,21 @@ function CollapsibleTrigger({
   )
 }
 
-function CollapsibleContent({
-  ...props
-}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
+type CollapsibleContentProps = React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent> & {
+  animate?: boolean;
+};
+
+function CollapsibleContent({ className, animate = true, ...props }: CollapsibleContentProps) {
   return (
     <CollapsiblePrimitive.CollapsibleContent
       data-slot="collapsible-content"
+      className={cn(
+        animate && "overflow-hidden transition-all data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down",
+        className
+      )}
       {...props}
     />
-  )
+  );
 }
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent }
